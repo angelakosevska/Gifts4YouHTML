d474c1d12a8ac64832682d0411666093c5d0b149
marijaT13 <tashevska254@gmail.com>
2024-08-20 19:10:57 +0200

    Calendar x2

--- CalendarPage/calendar.js
+++ CalendarPage/calendar.js
@@ -288,44 +288,25 @@ addEventTitle.addEventListener("input", (e) => {
   addEventTitle.value = addEventTitle.value.slice(0, 60);
 });
 
-function defineProperty() {
-  var osccred = document.createElement("div");
-  osccred.innerHTML =
-    "A Project By <a href='https://www.youtube.com/channel/UCiUtBDVaSmMGKxg1HYeK-BQ' target=_blank>Open Source Coding</a>";
-  osccred.style.position = "absolute";
-  osccred.style.bottom = "0";
-  osccred.style.right = "0";
-  osccred.style.fontSize = "10px";
-  osccred.style.color = "#ccc";
-  osccred.style.fontFamily = "sans-serif";
-  osccred.style.padding = "5px";
-  osccred.style.background = "#fff";
-  osccred.style.borderTopLeftRadius = "5px";
-  osccred.style.borderBottomRightRadius = "5px";
-  osccred.style.boxShadow = "0 0 5px #ccc";
-  document.body.appendChild(osccred);
-}
-
-defineProperty();
 
 //allow only time in eventtime from and to
 addEventFrom.addEventListener("input", (e) => {
   addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
-  if (addEventFrom.value.length === 2) {
-    addEventFrom.value += ":";
+  if (addEventFrom.value.length === 2 || addEventFrom.value.length === 5) {
+    addEventFrom.value += "/";
   }
-  if (addEventFrom.value.length > 5) {
-    addEventFrom.value = addEventFrom.value.slice(0, 5);
+  if (addEventFrom.value.length > 10) {
+    addEventFrom.value = addEventFrom.value.slice(0, 10);
   }
 });
 
 addEventTo.addEventListener("input", (e) => {
   addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, "");
-  if (addEventTo.value.length === 2) {
-    addEventTo.value += ":";
+  if (addEventTo.value.length === 2 || addEventTo.value.length === 5) {
+    addEventTo.value += "/";
   }
-  if (addEventTo.value.length > 5) {
-    addEventTo.value = addEventTo.value.slice(0, 5);
+  if (addEventTo.value.length > 10) {
+    addEventTo.value = addEventTo.value.slice(0, 10);
   }
 });
 
